d9446b2866fe5e576c06f00f78acb9ae9231b854
angelovangel <aangeloo@gmail.com>
2024-10-08 10:19:30 +0300

    fix perbase output

--- bin/perpos_freq.sh
+++ bin/perpos_freq.sh
@@ -3,7 +3,7 @@
 # calculate per base allele frequency from the output of `perbase base-depth`
 # then filter for potential heterozygous positions
 
-echo "POS DEPTH A C G T INS DEL"
-awk 'NR > 1 { print $2, $3, $4/$3, $5/$3, $6/$3, $7/$3, $9/$3, $10/$3 }' $1 | \
+echo -e "POS\tDEPTH\tA\tC\tG\tT\tINS\tDEL"
+awk -v OFS='\t' 'NR > 1 { print $2, $3, $4/$3, $5/$3, $6/$3, $7/$3, $9/$3, $10/$3 }' $1 | \
 # get suspicious positions only (heterozygous)
 awk 'NF>1 {for(i=3; i <= NF; i+=1) if ($i > 0.4 && $i < 0.6) print }' | uniq
