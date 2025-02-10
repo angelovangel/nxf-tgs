53ddadd44166e4535cf9a002a8d0e505469a5ce6
angelovangel <aangeloo@gmail.com>
2025-02-10 13:21:36 +0300

    fix failing samples missing in tables

--- bin/sample_status.R
+++ bin/sample_status.R
@@ -13,10 +13,19 @@ library(vroom)
 arg <- commandArgs(trailingOnly = T)
 tsvfiles <- list.files(path = ".", pattern = "*.assembly_stats.tsv", full.names = T)
 
-df1 <- vroom(arg[2], col_names = T, trim_ws = T, na = 'N/A') #! sample_status.txt
-colnames(df1) <- c('sample', 'pass_fail', 'length')
+df1 <- vroom(
+  arg[2], 
+  col_names = c('sample', 'pass_fail', 'length'), 
+  trim_ws = T, na = 'N/A'
+  ) #! sample_status.txt
 
-df2 <- vroom(arg[3], col_names = T, trim_ws = T)
+#samplesheet_validated
+df2 <- vroom(
+  arg[3], 
+  col_names = T, 
+  trim_ws = T, delim = ","
+  ) %>% 
+  dplyr::filter(user == arg[1])
 
 if (length(tsvfiles > 0)) {
   df3 <- vroom(
@@ -31,10 +40,10 @@ if (length(tsvfiles > 0)) {
   )
 }
 
-df1 %>% 
-  mutate(user = arg[1]) %>%
-  left_join(df2, by = join_by(user, sample)) %>%
-  dplyr::select(c('user', 'sample', 'barcode', user_size = 'dna_size', 'obs_size', assembly_size = 'length')) %>%
+df2 %>% 
+  #mutate(user = arg[1]) %>%
+  left_join(df1, by = join_by(sample)) %>%
+  dplyr::select(c('user', 'sample', 'validate','barcode', user_size = 'dna_size', 'obs_size', assembly_size = 'length')) %>%
   left_join(df3, by = c('sample' = 'sample_name')) %>%
   write.csv(file = 'sample-status.csv', row.names = F, col.names = T, sep = ",")
   
